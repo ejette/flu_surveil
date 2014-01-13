82f7468b319a8c8d0375f90faf4051e30861cab7
Jette Henderson <jette.henderson@gmail.com>
2014-01-13 10:52:06 -0600

    made logistic data input matrices

--- logistic/pop_data_prep.R
+++ logistic/pop_data_prep.R
@@ -1,11 +1,19 @@
 setwd("~/flu_surveil_data")
-
+library(plyr)
 # get list of counties
 load('counties.Rda')
 
 # get list of cities
-mmwr <- read.csv("mmwr.csv")
+mmwr <- read.csv("mmwr.csv",stringsAsFactors = FALSE )
 cities = count(mmwr[,c('city','state')])[,1:2]
+save(cities, file = '~/flu_surveil_data/cities.Rda')
+cities$city[cities$city == 'Boise' & cities$state == 'ID'] = 'Boise City'
+cities$city[cities$city == 'Colo. Springs' & cities$state == 'CO'] = 'Colorado Springs'
+cities$city[cities$city == 'Ft. Worth' & cities$state == 'TX'] = 'Fort Worth'
+cities$city[cities$city == 'Honolulu' & cities$state == 'HI'] = 'Urban Honolulu'
+cities$city[cities$city == 'New York City' & cities$state == 'NY'] = 'New York'
+
+# need "Elizabeth, NJ", newark, mj 124,969
 
 # load county information
 county_pop <- read.csv("~/Downloads/PEP_2012_PEPANNRES (1)/PEP_2012_PEPANNRES.csv")
@@ -51,6 +59,7 @@ counties$fips_format[counties$fips_format == 'RADFORD COUNTY, VA'] = "RADFORD CI
 counties$fips_format[counties$fips_format == 'SALEM COUNTY, VA'] = "SALEM CITY, VA"
 #counties$fips_format[counties$fips_format == 'UNORGANIZED COUNTY, AK'] = "
 counties$fips_format[counties$fips_format == 'VALDEZ CORDOVA COUNTY, AK'] = "VALDEZ-CORDOVA CENSUS AREA, AK"
+
 county_FIPS = merge(counties, FIPS, by.x = "fips_format", by.y = "county_form")
 
 cnt = counties$fips_format
@@ -60,6 +69,97 @@ missing = cnt[!(cnt %in% f)]
 
 m_county = merge(county_pop, county_FIPS, by.x = 'GEO.id2', by.y = 'FIPS_code')
 
+# cleaned 2010-2012 census data indexed by cbsa
+census_2010_to_2012_raw<-read.csv("~/repos/cdc_flu_contest/data/humidity/raw/CBSA-EST2012-01.csv", header=F, stringsAsFactors = FALSE)
+foo <- grepl('^[0-9]{5}$', census_2010_to_2012_raw[,1]) & (census_2010_to_2012_raw[,2]=='')
+census_2010_to_2012 <- census_2010_to_2012_raw[foo,-c(2,4:5)]
+colnames(census_2010_to_2012) <- c('cbsa','metro_name','2010','2011','2012')
+
+# split up metro_name into cities and state
+census_2010_to_2012$metro_name[463] = 'Canon City, CO'
+census_2010_to_2012$metro_name[537] = 'Espanola, NM'
+
+# split up further into separate cities and states
+list <- strsplit(census_2010_to_2012$metro_name, ",")
+df <- ldply(list)
+colnames(df) <- c("cities", "state")
+states_wide = as.data.frame(str_trim(str_split_fixed(df$state, '-', 4)), stringsAsFactors = FALSE)
+colnames(states_wide) = c('state1','state2','state3','state4')
+
+df$cities = gsub("--","-",df$cities)
+cities_wide = as.data.frame(str_trim(str_split_fixed(df$cities, '-', 4)),stringsAsFactors = FALSE)
+colnames(cities_wide) = c('city1','city2','city3','city4')
+wide = cbind(states_wide, cities_wide)
+census_city = cbind(census_2010_to_2012[,-2], wide)
+
+# make data long
+k = melt(census_city, id.vars = c('cbsa','2010','2011','2012','state1','state2','state3','state4'),measure.vars = c('city1','city2','city3','city4'))
+l = melt(k, id.vars = c('cbsa','2010','2011','2012','variable','value'), measure.vars =  c('state1','state2','state3','state4'))
+l = l[,-c(5,7)]
+colnames(l) = c('cbsa', '2010', '2011','2012','city','state')
+l$metro_name = paste(l$city, ', ', l$state, sep = '')
+
+cities$metro_name = paste(cities$city,', ', cities$state, sep = '')
+
+missing_cities = c("Elizabeth, NJ",
+                   "Lowell, MA",
+                   "Lynn, MA",
+                   "Pasadena, CA",
+                   "Paterson, NJ",
+                   "Somerville, MA",
+                   "Waterbury, CT",
+                   "Yonkers, NY",
+                   "Berkeley, CA",
+                   "Fall River, MA",
+                   "Gary, IN",
+                   "Glendale, CA",
+                   "New Bedford, MA")
+
+missing_pops = c(124969,
+                 106519,
+                 90329,
+                 137122,
+                 146199,
+                 75754,
+                 110366,
+                 195976,
+                 112580,
+                 88857,
+                 80294,
+                 191719,
+                 95072)
+
+empty = rep("",n)
+missing_obs = cbind(cbind(cbind(cbind(cbind(cbind(empty, missing_pops),empty),empty),empty),empty),missing_cities)
+colnames(missing_obs) = c('cbsa','2010','2011','2012','city','state','metro_name')
+l = rbind(l,missing_obs)
+
+m_cities = merge(cities, l, by = 'metro_name')
+
+c = cities$metro_name
+f = l$metro_name
+missing = c[!(c %in% f)]
+
+
+# calculate the humidity difference between the counties and the cities
+mat1 = as.data.frame(rep(m_county[1,'respop72010'],122))
+m_cities[,'2010'] = as.numeric(gsub(",","", m_cities[,'2010']))
+mat2 = as.data.frame(rep(m_cities[1,'2010'],nrow(m_county)))
+
+for (i in 2:nrow(m_county)){
+  mat1 = cbind(mat1, rep(m_county[i,'respop72010'],122))
+}
+
+for (i in 2:nrow(m_cities)){
+  mat2 = cbind(mat2, rep(m_cities[i,'2010'],nrow(m_county)))
+}
+
+mat1 = t(as.matrix(mat1))
+mat2 = as.matrix(mat2)
+
+pop_diff = mat2 - mat1
+
+save(pop_diff, file = '/Users/jj/flu_surveil_data/humidity/pop_diff.Rda')
 # extract population for counties
 #x = c('NY', 'AL','CT','DC')
 #state.name[match(x, state.abb)]
