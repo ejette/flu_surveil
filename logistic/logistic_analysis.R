6ffb03d16a7e1d451fd3484d68d1cfb097a7bab0
Jette Henderson <jette.henderson@gmail.com>
2014-01-17 15:10:38 -0600

    changed matrices into data frame

--- logistic/logistic_analysis.R
+++ logistic/logistic_analysis.R
@@ -1,61 +1,41 @@
 library(aod)
 library(ggplot2)
 
+source('~/flu_surveil/logistic/format_fips.R')
+
 setwd("~/Dropbox/122citites")
 # load y data
-load("~/flu_surveil_data/logistic_data/y.Rda")
+load("~/Dropbox/122citites/reg mats/y.Rda")
 # load humidity difference date
-load("~/flu_surveil_data/logistic_data/humidity_diff.Rda")
+load("~/Dropbox/122citites/reg mats/humidity_diff.Rda")
 # load population difference data
-load("~/flu_surveil_data/logistic_data/pop_diff.Rda")
+load("~/Dropbox/122citites/reg mats/pop_diff.Rda")
 # load geographic difference data
 load("~/Dropbox/122citites/reg mats/northSouthDist.Rda")
 # load geographic difference data
 load("~/Dropbox/122citites/reg mats/eastWestDist.Rda")
 
-load("~/Dropbox/122citites/reg mats/humidity_diff.Rda")
-load("~/Dropbox/122citites/y.Rda")
-load("~/Dropbox/122citites/reg mats/pop_diff.Rda")
 
 #pop_diff$FIPS_id = as.numeric(as.character(pop_diff$FIPS_id))
 
-# format FIPS code to be five characters long
-format_FIPS_id <- function(data){
-  for (i in 1:length(data)){
-    if (nchar(data[i]) < 5){
-      len = nchar(data[i])
-      data[i] = paste(paste(rep('0', 5 - len), collapse =''),data[i],sep='') 
-    }
-  }
-  return(data)
-}
-
-y$FIPS_id = format_FIPS_id(as.character(y$FIPS_code))
-humidity_diff$FIPS_id = format_FIPS_id(as.character(humidity_diff$FIPS_id))
-pop_diff$FIPS_id = format_FIPS_id(as.character(pop_diff$FIPS_id))
-NS[,1] = humidity_diff$FIPS_id
-EW[,1] = humidity_diff$FIPS_id
-
+#y$FIPS_code = format_fips(as.character(y$FIPS_code))
+colnames(y)[1] = 'FIPS_id'
 y$FIPS_num = as.numeric(y$FIPS_id)
+y = y[order(y$FIPS_num),]
+
 humidity_diff$FIPS_num = as.numeric(humidity_diff$FIPS_id)
+hum = humidity_diff[order(humidity_diff$FIPS_id),]
+
 pop_diff$FIPS_num = as.numeric(pop_diff$FIPS_id)
-EW = cbind(EW,as.numeric(EW[,1]))
-NS = cbind(NS,as.numeric(NS[,1]))
+pop = pop_diff[order(pop_diff$FIPS_num),]
 
-y = y[order(as.numeric(y$FIPS_num)),]
-humidity_diff = humidity_diff[order(humidity_diff$FIPS_num),]
-pop_diff = pop_diff[order(pop_diff$FIPS_num)),]
-NS = NS[order(NS[,124]),]
-EW = EW[order(EW[,124]),]
+EW_df = as.data.frame(EW, stringsAsFactors = FALSE)
+EW_df$FIPS_num = as.numeric(EW_df$FIPS_id)
+EW = EW_df[order(EW_df$FIPS_num),]
 
-y$FIPS_id = format_FIPS_id(as.character(y$FIPS_code))
-humidity_diff$FIPS_id = format_FIPS_id(as.character(humidity_diff$FIPS_id))
-pop_diff$FIPS_id = format_FIPS_id(as.character(pop_diff$FIPS_id))
-# There is something wrong with the way NS and EW FIPS_id was converted, but the rows are in the same 
-# order as humidity_diff, so we can use that
-
-
-
+NS_df = as.data.frame(NS, stringsAsFactors = FALSE)
+NS_df$FIPS_num = as.numeric(NS_df$FIPS_id)
+NS = NS_df[order(NS_df$FIPS_num),]
 
 # run one logistic regression
 # save p-values, coefficients
