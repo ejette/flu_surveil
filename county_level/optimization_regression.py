f11536b400fc7b312073b2b9d4e8e5f3fd2666b5
Jette Henderson <jette.henderson@gmail.com>
2014-01-23 15:24:48 -0600

    adapated code to run on data with missing values

--- county_level/optimization_regression.py
+++ county_level/optimization_regression.py
@@ -2,6 +2,7 @@ import time
 start_time = time.time()
 import pandas as pd
 import numpy as np
+import re
  
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -85,17 +86,51 @@ wd = '/Users/jj/flu_surveil_data/'
 
 candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
 del candidates_df['date']
+del candidates_df['row_num']
+
 # read in predicand
-deaths = pd.read_csv(wd+"flu_gold.csv", sep = " ")
-deaths = deaths['deaths']
+deaths_cities = pd.read_csv(wd+"flu_gold_all.csv", sep = " ")
+# remove the date column
+del deaths_cities['date']
+del deaths_cities['row_num']
 
-covar_n = 50
+cities = deaths_cities.columns.values
+city_names = [x[7:] for x in cities]
+city_names = [re.sub('\\.\\.', '\\.', x) for x in city_names]
+city_names = [re.sub(' ', '_', x) for x in city_names]
 
+deaths = deaths_cities[cities[0]]
+covar_n = 2
 results = optimize(deaths, candidates_df, covar_n)
 
-#for i in len(results['covars']):
-#    print results['covars'][i], results['r2_list'][i]
-print results['covars']
-print results['r2_list']
+# create a dataframe with the cumulative r^2 values
+r2_df = pd.DataFrame({city_names[0]: results['r2_list']})
+# create a dataframe with the county FIPS codes that the optimization picked
+covar_cols = results['covars'].columns.values
+covar_names = [x[6:] for x in covar_cols]
+covars_all_df = pd.DataFrame({city_names[0]: covar_names})
+
+n = len(city_names)
+
+for i in range(1,n):
+    print i
+    deaths = deaths_cities[cities[i]]
+    # check to see if there are missing values and remove them from the 
+    # predictor and predicand
+    present_indices = deaths.index[deaths.isnull() == False]
+    deaths = deaths.ix[present_indices]
+    candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
+    del candidates_df['date']
+    del candidates_df['row_num']
+    candidates_df = candidates_df.ix[present_indices,:]
+    results = optimize(deaths, candidates_df, covar_n)
+    r2_df[city_names[i]] = results['r2_list']
+    covar_cols = results['covars'].columns.values
+    covar_names = [x[6:] for x in covar_cols]
+    covars_all_df[city_names[i]] = covar_names
+
+
+covars_all_df.to_csv(wd + 'covars_cnty_w_zeros.csv')
+r2_df.to_csv(wd + 'r2s_cnty_w_zeros.csv')
 
 print "My program took", time.time() - start_time, "to run"
