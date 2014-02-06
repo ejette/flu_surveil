11d3636f4fb1b4f466bdb6a828e98e0c3e0eec24
Jette Henderson <jette.henderson@gmail.com>
2014-02-06 16:36:53 -0600

    updated program to handle regional data

--- county_level/optimization_regression.py
+++ county_level/optimization_regression.py
@@ -84,23 +84,24 @@ def optimize(deaths, candidates_df, covar_n):
     
 wd = '/Users/jj/flu_surveil_data/'
 
-candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
+#candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
+candidates_df = pd.read_csv(wd+"ili_flu_seasons.csv", sep=" ")
 del candidates_df['date']
-del candidates_df['row_num']
+#del candidates_df['row_num']
 
 # read in predicand
-deaths_cities = pd.read_csv(wd+"flu_gold_all.csv", sep = " ")
+deaths_cities = pd.read_csv(wd+"flu_gold_regions.csv", sep = " ")
 # remove the date column
 del deaths_cities['date']
-del deaths_cities['row_num']
+#del deaths_cities['row_num']
 
 cities = deaths_cities.columns.values
-city_names = [x[7:] for x in cities]
-city_names = [re.sub('\\.\\.', '\\.', x) for x in city_names]
-city_names = [re.sub(' ', '_', x) for x in city_names]
+city_names = ['region_' + x[7:] for x in cities]
+#city_names = [re.sub('\\.\\.', '\\.', x) for x in city_names]
+#city_names = [re.sub(' ', '_', x) for x in city_names]
 
 deaths = deaths_cities[cities[0]]
-covar_n = 2
+covar_n = 50
 results = optimize(deaths, candidates_df, covar_n)
 
 # create a dataframe with the cumulative r^2 values
@@ -111,7 +112,6 @@ covar_names = [x[6:] for x in covar_cols]
 covars_all_df = pd.DataFrame({city_names[0]: covar_names})
 
 n = len(city_names)
-
 for i in range(1,n):
     print i
     deaths = deaths_cities[cities[i]]
@@ -119,18 +119,22 @@ for i in range(1,n):
     # predictor and predicand
     present_indices = deaths.index[deaths.isnull() == False]
     deaths = deaths.ix[present_indices]
-    candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
+    #candidates_df = pd.read_csv(wd+"ili_wide_cnty_zeros_FIPS.csv", sep=" ")
+    candidates_df = pd.read_csv(wd+"ili_flu_seasons.csv", sep=" ")
     del candidates_df['date']
-    del candidates_df['row_num']
+    #del candidates_df['row_num']
     candidates_df = candidates_df.ix[present_indices,:]
     results = optimize(deaths, candidates_df, covar_n)
     r2_df[city_names[i]] = results['r2_list']
     covar_cols = results['covars'].columns.values
     covar_names = [x[6:] for x in covar_cols]
     covars_all_df[city_names[i]] = covar_names
+    covars_all_df.to_csv(wd + 'covars_cnty_to_region_w_zeros.csv')
+    r2_df.to_csv(wd + 'r2s_cnty_to_region_w_zeros.csv')
 
 
-covars_all_df.to_csv(wd + 'covars_cnty_w_zeros.csv')
-r2_df.to_csv(wd + 'r2s_cnty_w_zeros.csv')
+
+#covars_all_df.to_csv(wd + 'covars_cnty_w_zeros.csv')
+#r2_df.to_csv(wd + 'r2s_cnty_w_zeros.csv')
 
 print "My program took", time.time() - start_time, "to run"
